195fcc589c19426f0de74a20ad2e7419f60bee60
Bakkara <50306713+bakkara@users.noreply.github.com>
2024-02-02 00:17:45 +0200

    add styles 2

--- src/components/Psychologist/Psychologist.styled.js
+++ src/components/Psychologist/Psychologist.styled.js
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 export const PsycWrap = styled.div`
     border-radius: 24px;
-    background: rgb(251, 251, 251);
+    background: rgb(255, 255, 255);
     display: flex;
     gap: 24px;
     padding: 24px;
+    width: 100%;
 `
 
 export const AvatarWrap = styled.div`
@@ -17,12 +18,14 @@ export const AvatarWrap = styled.div`
 `
 
 export const DescriptionWrap = styled.div`
-
+    width: 100%;
 `
 export const HeadDescriptionWrap = styled.div`
     display: flex;
     align-items: center;
     margin-bottom: 8px;
+    width: 100%;
+    
 `
 export const PsyText = styled.p`
     color: rgb(138, 138, 137);
@@ -30,14 +33,18 @@ export const PsyText = styled.p`
     line-height: 1.5;
     letter-spacing: 0;
     text-align: left;
+    margin-right: auto;
 `
 
 export const RatingPriceBlock = styled.div`
-display: flex;
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: center;
+    margin-right: 28px;
 `
 
 export const RatingPriceText = styled.p`
-    color: rgb(25, 26, 21);
+    color: var(--black-color);
     font-weight: 500;
     line-height: 1.5;
     letter-spacing: 0;
@@ -48,15 +55,27 @@ export const RatingPriceText = styled.p`
         width: 0px;
         height: 16px;
         border: 1px solid rgba(25, 26, 21, 0.2);
-        margin-left: 4px;
-        margin-right: 4px;
+        margin-left: 16px;
+        margin-right: 16px;
     }
     
     &:last-child::after {
         content: none;
     }
 `
-
-
+export const RatingPriceTextSpan = styled.span`
+    color: var(--green-color);
+`
+export const SvgStar = styled.svg`
+    width: 16px;
+    height: 16px;
+    margin-right: 8px;
+`
+export const SvgHeart = styled.svg`
+    width: 26px;
+    height: 26px;
+    fill: none;
+    stroke: rgb(25, 26, 21);
+`
 
 
--- src/components/Psychologist/PsychologistCard.jsx
+++ src/components/Psychologist/PsychologistCard.jsx
@@ -1,5 +1,5 @@
-import { AvatarWrap, DescriptionWrap, HeadDescriptionWrap, PsyText, PsycWrap, RatingPriceBlock, RatingPriceText } from "./Psychologist.styled"
-
+import { AvatarWrap, DescriptionWrap, HeadDescriptionWrap, PsyText, PsycWrap, RatingPriceBlock, RatingPriceText, RatingPriceTextSpan, SvgHeart, SvgStar } from "./Psychologist.styled"
+import icons from '../../img/icons.svg';
 
 const PsychologistCard = () => {
   return (
@@ -9,9 +9,15 @@ const PsychologistCard = () => {
            <HeadDescriptionWrap>
             <PsyText>Psychologist</PsyText>
             <RatingPriceBlock>
+              <SvgStar>
+                <use href={`${icons}#icon-star`}></use>
+              </SvgStar>
               <RatingPriceText>Rating: 4.7</RatingPriceText>
-              <RatingPriceText>Price / 1 hour: 180$</RatingPriceText>
+              <RatingPriceText>Price / 1 hour: <RatingPriceTextSpan>180$</RatingPriceTextSpan> </RatingPriceText>
             </RatingPriceBlock>
+            <SvgHeart>
+                <use href={`${icons}#icon-heart`}></use>
+              </SvgHeart>
             </HeadDescriptionWrap> 
         </DescriptionWrap>    
     </PsycWrap>
