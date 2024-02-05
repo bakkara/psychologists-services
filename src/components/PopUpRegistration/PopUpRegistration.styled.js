2aa3004a239de651376d3296e6b74c5d6b13db30
Bakkara <50306713+bakkara@users.noreply.github.com>
2024-02-06 00:21:26 +0200

    Update PopUpRegistration.styled.js

--- src/components/PopUpRegistration/PopUpRegistration.styled.js
+++ src/components/PopUpRegistration/PopUpRegistration.styled.js
@@ -11,32 +11,34 @@ export const TextRegistration = styled.p`
 `
 
 export const Label = styled.label`
-  color: var(--black-color);
+  /* color: var(--black-color);
   font-size: 16px;
   font-weight: 400;
   line-height: 1.25;
   display: flex;
   flex-direction: column;
   gap: 8px;
-  margin-bottom: 16px;
+  margin-bottom: 16px; */
 `;
 
 export const StyledField = styled(Field)`
   display: flex;
-  padding: 12px 10px;
+  padding: 16px 18px;
   align-items: center;
   position: relative;
   width: 100%;
-  gap: 10px;
-  border-radius: 6px;
-  border: 1px solid #d7e3ff;
-  background: #fff;
-  color: #407bff;
+  border-radius: 12px;
+  border: 1px solid var(--normal-color);
+  color: var(--black-color);
+  margin-bottom: 40px;
+
+    &:last-child {
+    margin-bottom: 18px;
+    }
+
   &::placeholder {
-    color: #9ebbff;
-
+    color: var(--black-color);
     font-size: 16px;
-
     font-weight: 400;
     line-height: 1.25;
   }
@@ -47,28 +49,26 @@ export const StyledField = styled(Field)`
 
 export const Button = styled.button`
   display: inline-flex;
-  padding: 10px 30px;
+  padding: 16px 30px;
   justify-content: center;
   align-items: center;
   gap: 10px;
   color: #fff;
   text-align: center;
-  font-size: 18px;
+  font-size: 16px;
   font-weight: 500;
-  line-height: 1.33;
-  border-radius: 10px;
-  background: #407bff;
-  box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
+  line-height: 1.25;
+  border-radius: 30px;
+  background: var(--green-color);
   border: none;
   width: 100%;
-  margin-bottom: 16px;
   cursor: pointer;
 
   &:hover {
-    box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.54);
+    background: var(--hover-color);
   }
   &:focus {
-    box-shadow: 0px;
+    background: var(--hover-color);
   }
 `;
 
